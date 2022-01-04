f842166d3483720969f5b859407da925e50e6408
jccallo <jccallo@outlook.com>
2022-01-04 00:57:52 -0500

    generamos y configuramos el proyecto correctamente

---
+++ src/main/java/pe/jccallo/ThymeleafFundamentalsApplication.java
@@ -0,0 +1,13 @@
+package pe.jccallo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafFundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafFundamentalsApplication.class, args);
+	}
+
+}
